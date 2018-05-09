6544e89ca00871d848138f3d6042b3e42b6f3d8b
Kevin <16899282+kevinski303@users.noreply.github.com>
2018-05-09 15:57:42 +0200

    Update and rename alterMultipleDatabases.sql to setMultipleDatabasesOffline.sql

---
+++ setMultipleDatabasesOffline.sql
@@ -0,0 +1,22 @@
+DECLARE @sql VARCHAR(MAX), @dbName VARCHAR(25)
+
+DECLARE dbNameCursor CURSOR for
+	SELECT name from sys.databases WHERE name in (
+		'testDB1',
+		'testDB2',
+		'testDB3'
+	);
+
+OPEN dbNameCursor
+FETCH NEXT from dbNameCursor INTO @dbName 
+WHILE @@FETCH_STATUS = 0
+	BEGIN
+	SET @sql =  'ALTER DATABASE ' + @dbName + ' SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+				ALTER DATABASE '+ @dbName  + ' SET Offline;
+				ALTER DATABASE ' + @dbName + ' SET MULTI_USER;'
+	EXEC(@sql)
+	PRINT @sql + ' done'
+	FETCH NEXT from DbNameCursor INTO @dbName
+	END
+Close dbNameCursor
+Deallocate dbNameCursor
