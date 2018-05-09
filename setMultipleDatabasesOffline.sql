57d612899b473a587063911db024d6ddef72ae0d
Kevin <16899282+kevinski303@users.noreply.github.com>
2018-05-09 16:11:37 +0200

    Update setMultipleDatabasesOffline.sql

--- setMultipleDatabasesOffline.sql
+++ setMultipleDatabasesOffline.sql
@@ -11,9 +11,9 @@ OPEN dbNameCursor
 FETCH NEXT from dbNameCursor INTO @dbName 
 WHILE @@FETCH_STATUS = 0
 	BEGIN
-	SET @sql =  'ALTER DATABASE ' + @dbName + ' SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-				ALTER DATABASE '+ @dbName  + ' SET Offline;
-				ALTER DATABASE ' + @dbName + ' SET MULTI_USER;'
+	SET @sql =	'ALTER DATABASE ' + @dbName + ' SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+			ALTER DATABASE '+ @dbName  + ' SET Offline;
+			ALTER DATABASE ' + @dbName + ' SET MULTI_USER;'
 	EXEC(@sql)
 	PRINT @sql + ' done'
 	FETCH NEXT from DbNameCursor INTO @dbName
