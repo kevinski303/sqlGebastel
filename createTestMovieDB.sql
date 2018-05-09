2611ea6930d74cedbc4a07a8a4fbda0195eb54b3
Kevin <16899282+kevinski303@users.noreply.github.com>
2018-05-09 17:26:06 +0200

    Update createTestMovieDB.sql

--- createTestMovieDB.sql
+++ createTestMovieDB.sql
@@ -1,44 +1,56 @@
-DECLARE @dbname	varchar(45);
-DECLARE @tablename1 varchar(45);
-DECLARE @tablename2 varchar(45);
-DECLARE @tablename3 varchar(45);
+DECLARE @dbName	varchar(45);
+DECLARE @tableName1 varchar(45);
+DECLARE @tableName2 varchar(45);
+DECLARE @tableName3 varchar(45);
 
-DECLARE @sql_createdb VARCHAR(MAX);
-DECLARE @sql_createtable3 VARCHAR(MAX);
-DECLARE @sql_createtable2 VARCHAR(MAX);
-DECLARE @sql_createtable1 VARCHAR(MAX);
+DECLARE @sqlDropDatabase VARCHAR(MAX);
+DECLARE @sqlCreateDatabase VARCHAR(MAX);
+DECLARE @sqlCreateTable3 VARCHAR(MAX);
+DECLARE @sqlCreateTable2 VARCHAR(MAX);
+DECLARE @sqlCreateTable1 VARCHAR(MAX);
 
-DECLARE @sql_insertTable3 VARCHAR(MAX);
-DECLARE @sql_insertTable2 VARCHAR(MAX);
-DECLARE @sql_insertTable1 VARCHAR(MAX);
+DECLARE @sqlinsertTable3 VARCHAR(MAX);
+DECLARE @sqlinsertTable2 VARCHAR(MAX);
+DECLARE @sqlinsertTable1 VARCHAR(MAX);
 
-SET @dbname		= 'MovieDB'
-SET @tablename1 = 'Movies'
-SET @tablename2 = 'Genre'
-SET @tablename3 = 'Year'
+SET @dbName	= 'MovieDB'
+SET @tableName1 = 'Movies'
+SET @tableName2 = 'Genre'
+SET @tableName3 = 'Year'
 
-SET @sql_createdb = 'CREATE DATABASE ' + @dbname
-	EXECUTE (@sql_createdb)
-PRINT @sql_createdb
+USE [master];
+IF EXISTS(SELECT * FROM sys.databases where name = @dbName)
+	SET @sqlDropDatabase = 'DROP DATABASE ' + @dbName
+	EXECUTE(@sqlDropDatabase)
+	PRINT @sqlDropDatabase + ' -> Done'
 
-SET @sql_createtable3 = 'USE ' + @dbname + ' CREATE TABLE ' + @tablename3 
-	+ ' (ID int IDENTITY(1,1) PRIMARY KEY, ' + @tablename3 + ' VARCHAR(4) NOT NULL, UNIQUE(' + @tablename3 + '));'
-	EXECUTE (@sql_createtable3)
-PRINT @sql_createtable3 + ' Done'
+SET @sqlCreateDatabase = 'CREATE DATABASE ' + @dbName
+	EXECUTE (@sqlCreateDatabase)
+PRINT @sqlCreateDatabase + ' -> Done'
 
-SET @sql_createtable2 = 'USE ' + @dbname + ' CREATE TABLE ' + @tablename2 
-	+ ' (ID int IDENTITY(1,1) PRIMARY KEY, ' + @tablename2 + ' VARCHAR(45) NOT NULL, UNIQUE(Genre));'
-	EXECUTE (@sql_createtable2)
-PRINT @sql_createtable2 + ' Done'
+SET @sqlCreateTable3 = 'USE ' + @dbName + ' CREATE TABLE ' + @tableName3 
+	+ ' (ID int IDENTITY(1,1) PRIMARY KEY, ' + @tableName3 + ' INT NOT NULL, UNIQUE(' + @tableName3 + '));'
+	EXECUTE (@sqlCreateTable3)
+PRINT @sqlCreateTable3 + ' -> Done'
 
-SET @sql_createtable1 = 'USE ' + @dbname + ' CREATE TABLE ' + @tablename1 
-	+ ' (ID int IDENTITY(1,1) PRIMARY KEY, Name VARCHAR(MAX) NOT NULL, ' + @tablename2 + 'ID INT FOREIGN KEY REFERENCES ' + @tablename2 + '(ID), ' + @tablename3 + 'ID INT FOREIGN KEY REFERENCES ' + @tablename3 + '(ID));'
-	EXECUTE (@sql_createtable1)
-PRINT @sql_createtable1 + ' Done'
+SET @sqlCreateTable2 = 'USE ' + @dbName + ' CREATE TABLE ' + @tableName2 
+	+ ' (ID int IDENTITY(1,1) PRIMARY KEY, ' + @tableName2 + ' VARCHAR(45) NOT NULL, UNIQUE(' + @tableName2 + '));'
+	EXECUTE (@sqlCreateTable2)
+PRINT @sqlCreateTable2 + ' -> Done'
 
-/* ADD INSERTS
+SET @sqlCreateTable1 = 'USE ' + @dbName + ' CREATE TABLE ' + @tableName1 
+	+ ' (ID int IDENTITY(1,1) PRIMARY KEY, Name VARCHAR(MAX) NOT NULL, ' + @tableName2 + 'ID INT FOREIGN KEY REFERENCES ' + @tableName2 
+	+ '(ID), ' + @tableName3 + 'ID INT FOREIGN KEY REFERENCES ' + @tableName3 + '(ID));'
+	EXECUTE (@sqlCreateTable1)
+PRINT @sqlCreateTable1 + ' -> Done'
 
-SET @sql_insertTable3 = 'USE ' + @dbname + 'INSERT INTO ' + @tablename3
-	+
+/*INSERTS*/
+SET @sqlinsertTable3 = 'USE ' + @dbName + ' INSERT INTO ' + @tableName3 + '(' + @tableName3 + ') Values(1999),(2008),(2012);'
+PRINT @sqlinsertTable3 + ' -> Done'
 
-*/ 
+SET @sqlinsertTable2 = 'USE ' + @dbName + ' INSERT INTO ' + @tableName2 + '(' + @tableName2 + ') Values(''Thriller''),(''Action''),(''Fantasy'');'
+PRINT @sqlinsertTable2 + ' -> Done'
+
+SET @sqlinsertTable1 = 'USE ' + @dbName + ' INSERT INTO ' + @tableName1 + '(Name,' + @tableName2 + 'ID,' 
+	+ @tableName3 + 'ID) Values(''Inception'',1,3),(''Batman Begins'',2,2),(''Fight Club'',1,1);'
+PRINT @sqlinsertTable1 + ' -> Done'
